c5c8dd5e62a499a71ea025404bc80c7667343e59
BOSFramework <demo@bosframework.com>
2020-11-02 08:37:17 +0000

    Initial Commit

---
+++ src/BOS.StarterCode/Data/DBInitializer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using BOS.StarterCode.Models;
+namespace BOS.StarterCode.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(Context context)
+        {
+            //context.Database.EnsureCreated();
+            //Here seeding the data
+            //context.SaveChanges();
+        }
+    }
+}
